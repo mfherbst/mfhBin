3de3bc3dd2b3b79b2fae088c92db20b4d563f221
Michael F. Herbst <info@michael-herbst.com>
2016-03-13 11:06:21 +0100

    Cleanup of commandline options
    and adjustment of bash completion

---
+++ bash_completion.d/deltmp.bash
@@ -0,0 +1,35 @@
+_deltmp() {
+	local cur prefix comp_classes I lastarg
+	# the current word to be completed:
+	cur=${COMP_WORDS[COMP_CWORD]}
+	
+	# The current completion reply
+	COMPREPLY=()
+
+	compclasses="n"
+	# check if the completion is to complete classes
+	# or to complete arguments
+
+	if [ "$COMP_CWORD" -gt "1" ]; then
+		for ((I=COMP_CWORD-1;I>=0;--I)); do
+			lastarg="${COMP_WORDS[$I]}"
+			if [[ "$lastarg" == "--exclude" || "$lastarg" == "--only" || "$lastarg" == "-o" || "$lastarg" == "-e" ]]; then
+				compclasses="y"
+				break
+			fi
+		done
+	fi
+
+	if [ "$compclasses" == "y" ]; then
+		[ "$cur" == ":" ] && cur=""
+
+		COMPREPLY=( $( compgen -W 'comp mac nohup swp tilde win' -- "$cur" ) )
+		return 0
+	else 
+		COMPREPLY=( $( compgen -o plusdirs -W '-h --help -r --recursive --force -f --only -o --exclude -e --list -l' -- "$cur" ) )
+		return 0
+	fi
+	return 0
+}
+
+complete -F _deltmp deltmp
